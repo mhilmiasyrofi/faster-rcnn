728885356d543fa75644346a574a410ea7f78ea0
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2018-05-30 16:35:15 +0700

    add filepath

--- xml_to_kitti.py
+++ xml_to_kitti.py
@@ -7,6 +7,14 @@ if __name__ == '__main__':
         for idx, f in enumerate(sorted(os.listdir(path))) :
             tree = ET.parse(path + '/' + f)
             root = tree.getroot()
+            filename = root.find("filename").text
+            filepath = path + '/' + filename
             if idx == 0 :
                 # print(root.attrib)
-                
+                for child in root :
+                    if child.tag == "object" :
+                        print(child.find("name").text)
+                        # for ch in child
+                    # print(child.tag)
+
+
